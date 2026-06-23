1332951227a2247eaf9507dbad40792f898a1e9d
Theodor-Adrian Florescu <theodor_florescu@outlook.com>
2026-06-23 16:01:08 +0300

    Initial commit

---
+++ src/main/java/com/samplus/smartrecrutare/SmartrecrutareApplication.java
@@ -0,0 +1,13 @@
+package com.samplus.smartrecrutare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartrecrutareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartrecrutareApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/samplus/smartrecrutare/SmartrecrutareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.samplus.smartrecrutare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartrecrutareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
